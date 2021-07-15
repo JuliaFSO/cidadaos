10210e5182a684ea4caabd0ce491bfeb3615702f
julia fachin <juliafso@gmail.com>
2021-07-15 14:43:13 -0300

    feat(cidadao): criado show de munícipe

--- app/models/cidadao.rb
+++ app/models/cidadao.rb
@@ -1,9 +1,9 @@
 class Cidadao < ApplicationRecord
-  has_one_attached :foto
-  validates :nome_completo, :cpf, :email, :data_nascimento, :telefone, :status, presence: true
-  validates :cpf, length: { minimum: 11 }
-  validates :email, format: { with: /\A.*@.*\.com\z/ }
-  validates :telefone, length: { minimum: 13 }
+  # has_one_attached :foto
+  # validates :nome_completo, :cpf, :email, :data_nascimento, :telefone, :status, presence: true
+  # validates :cpf, length: { minimum: 11 }
+  # validates :email, format: { with: /\A.*@.*\.com\z/ }
+  # validates :telefone, length: { minimum: 13 }
   
-  validates :cpf, :email, uniqueness: true
+  # validates :cpf, :email, uniqueness: true
 end
