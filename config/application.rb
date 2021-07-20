165cb135e76018eb2a6dd150f908e93ff640d8c2
julia fachin <juliafso@gmail.com>
2021-07-20 12:04:45 -0300

    feat(locale): coloquei o locale ptBR pra deixar as coisas em português e inserir o i18n

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,15 @@ module Cidadaos
   class Application < Rails::Application
     config.action_view.embed_authenticity_token_in_remote_forms = true
     
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :de
+    
     config.generators do |generate|
       generate.assets false
       generate.helper false
       generate.test_framework :test_unit, fixture: false
     end
     # Initialize configuration defaults for originally generated Rails version.
-    Date::DATE_FORMATS[:default] = "%d/%m/%Y"
     config.load_defaults 6.0
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
